17e642a6b060b95fba9aec6ef0ecd778dee843b2
Your Name <you@example.com>
2026-01-07 21:13:08 +0100

    Add command for mac os user

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,1 +1,12 @@
-docker build -t sp500_db .
+#!/bin/bash
+set -e
+
+IMAGE_NAME=sp500_db
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  echo "Mac détecté → build en linux/amd64"
+  docker buildx build --platform linux/amd64 -t $IMAGE_NAME .
+else
+  echo "Linux détecté → build standard"
+  docker build -t $IMAGE_NAME .
+fi
